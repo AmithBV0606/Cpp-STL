390848429cda3427054db771d00fd5fe2ad6bd8c
AmithBV0606 <amithrao0606@gmail.com>
2025-01-18 20:44:33 +0530

    Studied about "at" method in vectors.

---
+++ 02_Vectors/Vector-Methods/At.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<char> v = {'a', 'c', 'f', 'd', 'z'};
+
+    // Accessing and printing values using indexes
+    cout << v[3] << endl; // d
+    cout << v.at(2);      // f
+
+    // Updating values using indexes 3 and 2
+    v[3] = 'D';
+    v.at(2) = 'F';
+
+    cout << v[3] << endl; // D
+    cout << v.at(2); // F
+
+    return 0;
+}
