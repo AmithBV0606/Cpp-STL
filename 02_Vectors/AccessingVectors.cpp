6469e638397709c6f34ee56e94129e54b7b22ac5
AmithBV0606 <amithrao0606@gmail.com>
2025-01-18 19:57:03 +0530

    Added comments.

--- 02_Vectors/AccessingVectors.cpp
+++ 02_Vectors/AccessingVectors.cpp
@@ -7,9 +7,14 @@ int main()
     v.push_back(1);
     v.emplace_back(2);
 
+    // Normal way : 
+    cout << v[1] << v[2];
+
+    // Using iterators : 
     vector<int>::iterator it = v.begin();
-    it++;
+    // v.begin() gives the starting memory location of the Vector "v".
     cout << *(it) << " ";
+    it++;
     vector<int>::iterator it = v.end();
     return 0;
 }
--- 02_Vectors/Vectors.cpp
+++ 02_Vectors/Vectors.cpp
@@ -3,18 +3,22 @@ using namespace std;
 
 int main()
 {
-    vector<int> v;
-    v.push_back(1);
-    v.emplace_back(2);
+    vector<int> v; // Creates an empty container
+    v.push_back(1); // v = {1}
+    v.emplace_back(2); // v = {1, 2}
 
+    // Vectors of pair data types : 
     vector<pair<int, int>> vec;
     vec.push_back({1, 2});
     vec.emplace_back(1, 2);
 
+    // Creates a vector of size 5 and fills it with the number 100.
     vector<int> vec2(5, 100);
 
+    // Creates a vector of size 5.
     vector<int> vec3(5);
 
+    // Creates a vector of size 5 and fills it with the number 20.
     vector<int> vec4(5, 20);
 
     vector<int> vec5(vec4);
