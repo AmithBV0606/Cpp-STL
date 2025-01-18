2735244b5172706964bb368bb9baea1dd882f7bd
AmithBV0606 <amithrao0606@gmail.com>
2025-01-18 20:48:35 +0530

    Studied about "push_back" method in c++.

---
+++ 02_Vectors/Vector-Methods/push_back.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<char> v3 = {'a', 'f', 'd'};
+
+    // Inserting 'z' at the back
+    v3.push_back('z');
+    return 0;
+}
