19daeb6c3f34bf06f997cf01608badd3e25e160d
AmithBV0606 <amithrao0606@gmail.com>
2025-01-18 22:54:57 +0530

    Studied about"erase" method in vectors.

---
+++ 02_Vectors/Vector-Methods/Erase.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v6 = {1, 2, 3, 4, 5, 6, 7, 8};
+
+    v6.erase(v6.begin() + 1); // 2 will be removed
+
+    return 0;
+}
