26feb6b4580616911609cbffa3baf33735f63e46
AmithBV0606 <amithrao0606@gmail.com>
2025-01-18 20:51:37 +0530

    Studied about "pop_back" method is vectors.

---
+++ 02_Vectors/Vector-Methods/pop_back.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<char> v4 = {'a', 'c', 'f', 'd', 'z'};
+
+    // Deleting last element 'z'
+    v4.pop_back();
+    for (int i = 0; i < v4.size(); i++)
+    {
+        cout << v4[i] << " ";
+    }
+    cout << endl;
+
+    // Output : a c f d
+
+    return 0;
+}
